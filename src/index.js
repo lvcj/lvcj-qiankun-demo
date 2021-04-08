5e071665e267227633eb6d1ebe93f82024cdcfa1
吕春静 <lvchunj@chanjet.com>
2021-04-09 07:44:52 +0800

    乾坤子组件的设置

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom"
-
+import {history} from "~/store/index"
 import { registerMicroApps, runAfterFirstMounted, setDefaultMountApp, start, initGlobalState} from "qiankun"
 import Entry from "./entry"
 ReactDOM.render(<Entry />, document.getElementById("root-main"))
@@ -15,14 +15,45 @@ function render({ appContent, loading }) {
     ReactDOM.render(<Render appContent={appContent} loading={loading} />, container);
 }
 
+
+function genActiveRule(routerPrefix) {
+  return (location) => {
+    // '/foo'
+    if (typeof routerPrefix === 'string') {
+      if (routerPrefix[0] === '#') {
+        return location.hash.startsWith(routerPrefix);
+      }
+      return location.pathname.startsWith(routerPrefix);
+    }
+
+    // /^\/(foo|bar|baz)/
+    if (routerPrefix instanceof RegExp) {
+      return routerPrefix.test(location.pathname);
+    }
+    // ['/foo', '/bar']
+    if (Array.isArray(routerPrefix)) {
+      return routerPrefix.some((prefix) => {
+        if (prefix[0] === '#') {
+          return location.pathname === '/' && location.hash.startsWith(prefix);
+        }
+        return location.pathname.startsWith(prefix);
+      });
+    }
+  };
+}
+
 registerMicroApps(
     [
       {
         name: 'react16',
         entry: 'http://localhost:8088/',
         container: '#subapp-viewport',
-        render,
-        activeRule: '/about-react',
+        //render,
+        activeRule: genActiveRule(['/mshop-about-react','/mshop-about-react1']),
+        props: {
+          name: 'kuitos',
+          history: history,
+        }
       }
     ],
     {
@@ -57,8 +88,11 @@ registerMicroApps(
    */
   start({
     prefetch: [],
-    sandbox: true,
+    sandbox: {strictStyleIsolation:true},
     singular: true,
+    // getTemplate:function(template){
+    //   console.log("template===>>>>", template)
+    // }
   });
   
   runAfterFirstMounted(() => {
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,8 +1,9 @@
 import React from "react";
+import "./index.scss"
 
 export default function Home(props){
     const onclick = ()=>{
-        props.history.push(`/about-react`)
+        props.history.push(`/mshop-about-react`)
     }
-    return <div onClick={onclick}>hello home qiankun</div>
+    return <div onClick={onclick} styleName="home-wrap">hello home qiankun</div>
 }
